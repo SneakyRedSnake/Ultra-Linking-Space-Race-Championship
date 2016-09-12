f565e3fbf7f6b03f2dde7f1a4d1191fb55f3c503
Michael Eusebe <michael.eusebe@etu.unice.fr>
2016-09-12 23:42:04 +0200

    add a first version of player controller

---
+++ UltraLinkingSpaceRaceChampionShip/Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+///     Controller to retrieve the player inputs
+///     and send them to the components that need them
+///     (e.g. : movement, attack, ...)
+/// </summary>
+public class PlayerController : MonoBehaviour {
+
+	/// <summary>
+    ///     Retrieve the axis translation of the player, and send it to the Move component
+    /// </summary>
+	void Update () {
+        float verticalTranslation = Input.GetAxis("Vertical");
+        float horizontalTranslation = Input.GetAxis("Horizontal");
+
+        //component.move(horizontal, vertical);
+    }
+}

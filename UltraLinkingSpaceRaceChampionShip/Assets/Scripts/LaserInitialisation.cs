49394bb9fbd3a6bfd617174154cc1de5d4adc5a9
Michael Eusebe <michael.eusebe@etu.unice.fr>
2016-09-14 00:43:54 +0200

    Add Laser Prefab && LaserInitialisation

---
+++ UltraLinkingSpaceRaceChampionShip/Assets/Scripts/LaserInitialisation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserInitialisation : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject child1;
+
+    [SerializeField]
+    private GameObject child2;
+
+    // Use this for initialization
+    void Start () {
+        Vector3 pos = gameObject.GetComponent<Transform>().position;
+        Vector3 size = gameObject.GetComponent<Transform>().localScale;
+        Quaternion rot = gameObject.GetComponent<Transform>().rotation;
+
+
+        child1.GetComponent<Transform>().position = /*Reloud!!!*/new Vector3();
+        child2.GetComponent<Transform>().position = /*Reloud!!!*/new Vector3();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

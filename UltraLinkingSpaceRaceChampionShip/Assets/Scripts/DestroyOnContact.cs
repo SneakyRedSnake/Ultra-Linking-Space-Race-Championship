602b130591a53360c7ae9deb5d8a4d1ed1f3e833
Lydwen <tomlau94@aol.com>
2016-09-14 00:45:23 +0200

    Add a component that destroys gameobject that hit the owner of this component

---
+++ UltraLinkingSpaceRaceChampionShip/Assets/Scripts/DestroyOnContact.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+///     Destroy every gameobject that enter in contact
+///     with the game object that possess this component
+/// </summary>
+public class DestroyOnContact : MonoBehaviour {
+
+    /// <summary>
+    ///     When a gameobject collide with the owner
+    ///     of this component, it destroy himself
+    /// </summary>
+    /// <param name="col">the Collision2D of the gameobject
+    /// that hits the owner of this component</param>
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(col.gameObject);
+    }
+}
